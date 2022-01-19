613359a59a4a623398bbcab95a0f34656c2511ba
Runchi <runchidasemail@gmail.com>
2022-01-19 19:06:47 +0100

    - add scroll to top option at the bottom

--- js/app.js
+++ js/app.js
@@ -1,61 +1,108 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
+window.scroll(0, 0);
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
+// Global variables
+const contents = document.querySelectorAll('.landing__container');
+let sectionPositions = [];
+let sectionThresholds = [];
+let clicked = false;
 
-/**
- * Define Global Variables
- * 
-*/
+// calculate Y-threshold for active sections
+let sumHeight = 0;
+for (let i = 0; i < contents.length; i++) {
+    sectionPositions.push(contents[i].getBoundingClientRect());
+    sectionThresholds.push(sumHeight + sectionPositions[i].height);
+    sumHeight += sectionPositions[i].height;
+}
 
+// Feature 1: Build dynamic navigation bar
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
+// query after content (landing-container class)
 
+// create document fragment to store nav content
+const navFragment = document.createDocumentFragment();
 
+// create list element from contents
+for (let i = 0; i < contents.length; i++) {
+    const navContent = document.createElement('li');
+    navContent.innerHTML = contents[i].getElementsByTagName('h2')[0].textContent;
+    navContent.setAttribute('id', `nav${i + 1}`);
+    if (i == 0) {
+        navContent.setAttribute('class', 'your-active-class');
+    }
+    navFragment.append(navContent);
+}
+const navBar = document.getElementById('navbar__list');
+navBar.appendChild(navFragment);
+// END Feature 1
 
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
+// Feature 2: Scrolling to section on click
+navBar.addEventListener('click', navOnClick);
 
-// build the nav
+// Change styles and scroll to section
+function navOnClick(event) {
+    clicked = true;
+    event.preventDefault();
+    if (event.target.tagName == 'LI') {
+        const sectionNr = (event.target.textContent).split(' ')[1];
+        const newActive = changeActiveElements(sectionNr);
+        newActive.scrollIntoView({ block: "start", behavior: "smooth" });
+        // newActive.scroll({ block: "start", behavior: "smooth" });
 
+    }
+    setTimeout(func => { clicked = false }, 1000);
+}
 
-// Add class 'active' to section when near top of viewport
+function changeActiveElements(sectionNr) {
+    const currentActives = document.querySelectorAll('.your-active-class');
+    const newActiveSection = document.querySelector(`#section${sectionNr}`);
+    const newActiveNav = document.querySelector(`#nav${sectionNr}`);
 
+    for (let i = 0; i < currentActives.length; i++) {
+        currentActives[i].classList.toggle('your-active-class');
+    }
 
-// Scroll to anchor ID using scrollTO event
+    newActiveSection.classList.toggle('your-active-class');
+    newActiveNav.classList.toggle('your-active-class');
+    activeSection = newActiveSection;
+    return newActiveSection;
+}
+// END Feature 2
 
+// Feature 3: Detect active section
+document.addEventListener('scroll', onScroll);
 
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
+function onScroll() {
+    detectActiveRegion();
+    checkScrollY();
+}
 
-// Build menu 
+function detectActiveRegion() {
+    if (!clicked) {
+        for (let i = 0; i < sectionThresholds.length; i++) {
+            if (window.scrollY < (sectionThresholds[i])) {
+                changeActiveElements(i + 1);
+                break;
+            }
+        }
+    }
+}
 
-// Scroll to section on link click
+// if the page bottom is reached, toggle to the top option
+function checkScrollY() {
+    if (window.scrollY > window.scrollMaxY - 50) {
+        document.getElementById('link-to-top').classList.add('visible');
+    }
+    else {
+        document.getElementById('link-to-top').classList.remove('visible');
+    }
+}
+// END Feature 3
 
-// Set sections as active
+// scroll to top button
+document.querySelector('#link-to-top').addEventListener('click', scrollToTop);
 
+function scrollToTop() {
+    clicked = true;
+    window.scrollTo({top: 0, block: "start", behavior: "smooth" });
+    clicked = false;
+}
